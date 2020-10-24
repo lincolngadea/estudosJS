6bfa4e22ebd2b5ecc16d0cde44e8a9a5e473dd60
Lincoln Gadea <lincoln@glinc.com.br>
2020-10-24 14:50:01 -0300

    Update: Melhora a listagem de atividades

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+let body = document.querySelector('.title');
+let title = document.createElement('h2');
+let descTitle = document.createTextNode('ESTRUTURA CRIADA NO JAVASCRIPT');
+
+title.appendChild(descTitle);
+body.appendChild(title)
+
+
 let ul = document.createElement('ul');
 
 function insertLi(link,listText){
+
     let list = document.querySelector('.list');
     let li = document.createElement('li');
-    let listAdd = document.createTextNode(listText);
+    //let listAdd = document.createTextNode(listText);
 
     let linkElement = document.createElement('a');
     linkElement.setAttribute('href',link);    
@@ -15,7 +24,10 @@ function insertLi(link,listText){
     list.appendChild(ul);
 }
 
-
-insertLi("https://lincolngadea.com.br",'texto');
-insertLi("https://lincolngadea.com.br",'texto2');
+insertLi("/desafio01.html",'Desafio 01');
+insertLi("/desafio02.html",'Desafio 02');
+insertLi("/desafio03.html",'Desafio 03');
+insertLi("/desafio04.html",'Desafio 04');
+insertLi("/desafio05.html",'Desafio 05');
+insertLi("/desafio06.html",'Desafio 06');
 
--- script4.js
+++ script4.js
@@ -1,3 +1,18 @@
+let div = document.querySelector('.rec');
+let dataInput = document.createElement('input');
+
+div.appendChild(dataInput);
+
+let btn = document.createElement('button');
+btn.setAttribute('type','button');
+btn.appendChild(document.createTextNode('Verificar'));
+div.appendChild(btn);
+
+btn.onclick = ()=>{
+     let anosEstudo = document.querySelector('input').value;
+     experiencia(anosEstudo);
+ };
+    
 function experiencia(anos) {
     // código aqui
     if(anos > 0 && anos <=1){
@@ -10,9 +25,9 @@ function experiencia(anos) {
         console.log("Jedi Master");
     }
 }
-   var anosEstudo = 4;
-   experiencia(anosEstudo);
-   // De 0-1 ano: Iniciante
-   // De 1-3 anos: Intermediário
-   // De 3-6 anos: Avançado
-   // De 7 acima: Jedi Master
+   
+    
+    // De 0-1 ano: Iniciante
+    // De 1-3 anos: Intermediário
+    // De 3-6 anos: Avançado
+    // De 7 acima: Jedi Master
